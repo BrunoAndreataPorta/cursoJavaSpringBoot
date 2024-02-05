b2e5abebdeb199c52f5e2cf9f52a59cb718aa5e9
BrunoAPorta <Bruno.porta@hotmail.com>
2024-02-05 13:24:48 -0300

    Entendendo o Spring Framework

---
+++ SpringFramework/SpringDemo/src/main/java/com/cursojava/Alien.java
@@ -0,0 +1,40 @@
+package com.cursojava;
+
+public class Alien {
+
+    private int age;
+    private Laptop lap;
+
+    public Alien()
+    {
+        System.out.println("Object Created!");
+    }
+
+    public Alien(int age){
+        System.out.println("Param Constructor Called");
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Laptop getLap() {
+        return lap;
+    }
+
+    public void setLap(Laptop lap) {
+        this.lap = lap;
+    }
+
+
+    public void code()
+    {
+        System.out.println("Coding...");
+        lap.compile();
+    }
+}
---
+++ SpringFramework/SpringDemo/src/main/java/com/cursojava/Laptop.java
@@ -0,0 +1,11 @@
+package com.cursojava;
+
+public class Laptop {
+    public Laptop() {
+        System.out.println("Laptop Obj Created");
+    }
+
+    public void compile(){
+        System.out.println("Compiling...");
+    }
+}
