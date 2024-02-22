2ca2340a0f27f9b313f1fe72aa654bb91596e580
BrunoAPorta <Bruno.porta@hotmail.com>
2024-02-22 12:20:19 -0300

    avanco na secao Spring Boot Web

---
+++ ServletEx/src/main/java/com/cursojava/HelloServlet.java
@@ -0,0 +1,20 @@
+package com.cursojava;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        System.out.println("In server");
+
+        res.setContentType("text/html");
+
+        PrintWriter out = res.getWriter();
+        out.println("<h2>Hello Friend<h2>");
+    }
+
+}
